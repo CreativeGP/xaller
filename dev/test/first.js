29153e7aa90c4c1ff4580d598db754f156e0d145
CreativeGP <malicktafaminecraft@gmail.com>
2017-09-19 22:29:40 +0900

    タイポ修正

--- dev/test/first.js
+++ dev/test/first.js
@@ -1,9 +1,21 @@
 $(function() {
 	;
-	;
+	return ('ss +  '3333);
 }
-;
-;
+var btn = '';
+var btn$_web = '';
+var btn$text = '';
+var btn$name = '';
+function btn$__init() {
+	btn$text = 'Button from xaller.';
+	$('#btn').html(btn$text);
+	btn$_web = 'Button';
 }
+btn$text = 'きよみ';
+$('#btn').html(btn$text);
+$('#btn').hover(function () {
+	btn$text = 'きよみが押しました';
+	$('#btn').html(btn$text);
+});
 return;
 });
