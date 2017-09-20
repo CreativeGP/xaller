219ce3d4b3bb10dab8579a98baba667903a3dec1
CreativeGP <malicktafaminecraft@gmail.com>
2017-09-21 07:08:22 +0900

    とりあえずのの安定動作

--- dev/test/calc.js
+++ dev/test/calc.js
@@ -1,18 +1,230 @@
-$('body').append("<textarea id='view' name='view'></textarea>");
-$("#view").attr('_web', 'Textbox');
-var i;
-while (true) {
-$("#button1").html("1");
-$("#button2").html("2");
-$("#button3").html("3");
-$("#button4").html("4");
-$("#button5").html("5");
-$("#button6").html("6");
-$("#button7").html("7");
-$("#button8").html("8");
-$("#button9").html("9");
-}
-$('body').append("<button type='button' id='eq_btn'></button>");
-$("#eq_btn").html("=");
-$('body').append("<button type='button' id='plus_btn'></button>");
-$("#plus_btn").html("+");
+$(function() {
+	var view = '';
+	var view$_web = '';
+	var view$rows = 0;
+	var view$cols = 0;
+	var view$text = '';
+	{
+		view$_web = 'Textbox';
+	}
+	$('body').append("<textarea id='view' name='view'></textarea>");
+	var button1 = '';
+	var button1$_web = '';
+	var button1$text = '';
+	var button1$name = '';
+	var button1$num = 0;
+	{
+		button1$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button1'></button>");
+	function button1$set_num(num) {
+		button1$num = num;
+		button1$text = Number(num);
+		$('#button1').html(button1$text);
+	}
+	$('#button1').click(function () {
+		view$text = ($('#view').html() + String((button1$num)));
+		$('#view').html(view$text);
+	});
+	button1$set_num(1);
+	var button2 = '';
+	var button2$_web = '';
+	var button2$text = '';
+	var button2$name = '';
+	var button2$num = 0;
+	{
+		button2$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button2'></button>");
+	function button2$set_num(num) {
+		button2$num = num;
+		button2$text = Number(num);
+		$('#button2').html(button2$text);
+	}
+	$('#button2').click(function () {
+		view$text = ($('#view').html() + String((button2$num)));
+		$('#view').html(view$text);
+	});
+	button2$set_num(2);
+	var button3 = '';
+	var button3$_web = '';
+	var button3$text = '';
+	var button3$name = '';
+	var button3$num = 0;
+	{
+		button3$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button3'></button>");
+	function button3$set_num(num) {
+		button3$num = num;
+		button3$text = Number(num);
+		$('#button3').html(button3$text);
+	}
+	$('#button3').click(function () {
+		view$text = ($('#view').html() + String((button3$num)));
+		$('#view').html(view$text);
+	});
+	button3$set_num(3);
+	var button4 = '';
+	var button4$_web = '';
+	var button4$text = '';
+	var button4$name = '';
+	var button4$num = 0;
+	{
+		button4$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button4'></button>");
+	function button4$set_num(num) {
+		button4$num = num;
+		button4$text = Number(num);
+		$('#button4').html(button4$text);
+	}
+	$('#button4').click(function () {
+		view$text = ($('#view').html() + String((button4$num)));
+		$('#view').html(view$text);
+	});
+	button4$set_num(4);
+	var button5 = '';
+	var button5$_web = '';
+	var button5$text = '';
+	var button5$name = '';
+	var button5$num = 0;
+	{
+		button5$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button5'></button>");
+	function button5$set_num(num) {
+		button5$num = num;
+		button5$text = Number(num);
+		$('#button5').html(button5$text);
+	}
+	$('#button5').click(function () {
+		view$text = ($('#view').html() + String((button5$num)));
+		$('#view').html(view$text);
+	});
+	button5$set_num(5);
+	var button6 = '';
+	var button6$_web = '';
+	var button6$text = '';
+	var button6$name = '';
+	var button6$num = 0;
+	{
+		button6$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button6'></button>");
+	function button6$set_num(num) {
+		button6$num = num;
+		button6$text = Number(num);
+		$('#button6').html(button6$text);
+	}
+	$('#button6').click(function () {
+		view$text = ($('#view').html() + String((button6$num)));
+		$('#view').html(view$text);
+	});
+	button6$set_num(6);
+	var button7 = '';
+	var button7$_web = '';
+	var button7$text = '';
+	var button7$name = '';
+	var button7$num = 0;
+	{
+		button7$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button7'></button>");
+	function button7$set_num(num) {
+		button7$num = num;
+		button7$text = Number(num);
+		$('#button7').html(button7$text);
+	}
+	$('#button7').click(function () {
+		view$text = ($('#view').html() + String((button7$num)));
+		$('#view').html(view$text);
+	});
+	button7$set_num(7);
+	var button8 = '';
+	var button8$_web = '';
+	var button8$text = '';
+	var button8$name = '';
+	var button8$num = 0;
+	{
+		button8$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button8'></button>");
+	function button8$set_num(num) {
+		button8$num = num;
+		button8$text = Number(num);
+		$('#button8').html(button8$text);
+	}
+	$('#button8').click(function () {
+		view$text = ($('#view').html() + String((button8$num)));
+		$('#view').html(view$text);
+	});
+	button8$set_num(8);
+	var button9 = '';
+	var button9$_web = '';
+	var button9$text = '';
+	var button9$name = '';
+	var button9$num = 0;
+	{
+		button9$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button9'></button>");
+	function button9$set_num(num) {
+		button9$num = num;
+		button9$text = Number(num);
+		$('#button9').html(button9$text);
+	}
+	$('#button9').click(function () {
+		view$text = ($('#view').html() + String((button9$num)));
+		$('#view').html(view$text);
+	});
+	button9$set_num(9);
+	var button0 = '';
+	var button0$_web = '';
+	var button0$text = '';
+	var button0$name = '';
+	var button0$num = 0;
+	{
+		button0$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='button0'></button>");
+	function button0$set_num(num) {
+		button0$num = num;
+		button0$text = Number(num);
+		$('#button0').html(button0$text);
+	}
+	$('#button0').click(function () {
+		view$text = ($('#view').html() + String((button0$num)));
+		$('#view').html(view$text);
+	});
+	button0$set_num(0);
+	var eq_btn = '';
+	var eq_btn$_web = '';
+	var eq_btn$text = '';
+	var eq_btn$name = '';
+	{
+		eq_btn$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='eq_btn'></button>");
+	eq_btn$text = '=';
+	$('#eq_btn').html(eq_btn$text);
+	$('#eq_btn').click(function () {
+		var a = '';
+		a = ($('#view').html()  3  5);
+	});
+	var plus_btn = '';
+	var plus_btn$_web = '';
+	var plus_btn$text = '';
+	var plus_btn$name = '';
+	{
+		plus_btn$_web = 'Button';
+	}
+	$('body').append("<button type='button' id='plus_btn'></button>");
+	plus_btn$text = '+';
+	$('#plus_btn').html(plus_btn$text);
+	$('#plus_btn').click(function () {
+		view$text = ($('#view').html() + '+');
+		$('#view').html(view$text);
+	});
+	return;
+});
